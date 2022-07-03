75b328b11cc18c1b4492dd413b3c814e7fdfdb2b
lenevdm <lenebooysen@gmail.com>
2022-07-03 14:53:21 +0200

    Front-end

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Device table functions
+document.addEventListener('DOMContentLoaded', function(){
+  loadHTMLTable([]);
+});
+function loadHTMLTable(data){
+  const table = document.querySelector('table tbody');
+  
+  if (data.length == 0){
+    table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+  }
+};
+
 // Set up Express and create an app to run on port 8089
 const express = require('express');
 const app = express();
